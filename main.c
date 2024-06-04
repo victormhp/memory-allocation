f21bc0833afc797472b16f828b5bd3e9213bbf30
victormhp <victoralbertomhp@gmail.com>
2024-06-04 12:06:02 -0600

    Test allocation

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include "mem_allocation.h"
 #include <stdio.h>
 
-void test_dummy_malloc()
+void test_alloc()
 {
     size_t size = 1024;
     void* initial_break = sbrk(0);
     printf("Initial break: %p\n", initial_break);
 
-    void* allocated_memory = dummy_malloc(size);
+    void* allocated_memory = heap_alloc(size);
     if (allocated_memory == NULL) {
         printf("Memory allocation failed!\n");
     }
@@ -16,9 +16,12 @@ void test_dummy_malloc()
     printf("Allocated memory starts at: %p\n", allocated_memory);
     printf("New program break: %p\n", new_break);
     printf("Expected new break: %p\n", (char*)initial_break + size);
+
+    heap_free(allocated_memory);
+    printf("Free break: %p\n", initial_break);
 }
 
 int main()
 {
-    test_dummy_malloc();
+    test_alloc();
 }
